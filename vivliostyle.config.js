a08c08fbfed1eb22ba5aea83a0c5db11551e304f
Yuki Fujiwara <sky.y.0079@gmail.com>
2020-12-24 22:59:36 +0900

    initial commit

---
+++ vivliostyle.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: '科学の不思議', 
+  author: 'アンリイ・ファブル（大杉栄、伊藤野枝訳）', 
+  size: 'JIS-B5', // paper size.
+  theme: '@vivliostyle/theme-techbook', // .css or local dir or npm package. default to undefined.
+  entry: [
+    '01.md',
+  ], 
+}
